dafc883463bf4113039d6f8c5f07e1e108a7ce01
t1053600121@unitru.edu.pe <t1053600121@unitru.edu.pe>
2025-12-16 17:00:15 +0000

    Created ServerModule1, login.png and logoUNT.jpeg; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,18 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_login_click(self, **event_args):
+    user_name = self.name.text
+    user_password = self.password.text
+    if user_name != "" and user_password != "":
+      alert(f"Hola, {user_name}")
+    else:
+      alert("Access denied")
+
+  def button_create_click(self, **event_args):
+    user_name = self.name.text
+    user_password = self.password.text
+    resp = anvil.server.call('create_user', user_name, user_password)
+    if resp:
+      alert(f"El usuario {user_name} ha sido creado")
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,16 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+@anvil.server.callable
+def create_user(user_name, user_password):
+  app_tables.users.add_row(name = user_name, password=user_password)
+  return True
